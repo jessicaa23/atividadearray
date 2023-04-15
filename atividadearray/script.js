d04dc76dcf006ada2372b5f07f5f1aaf53087f51
jessicasilva <109664683+jessicaa23@users.noreply.github.com>
2023-04-15 11:25:23 -0300

    Add files via upload

---
+++ atividadearray/script.js
@@ -0,0 +1,12 @@
+//const names = ['jenifer', 'andrielly', 'leticia'];
+//names.push('jessica');
+//console.log(names);
+
+//const animal = ['gato', 'cachorro', 'tigre', 'tubarão'];
+//animal.pop();
+//console.log(animal)
+
+const fruit = ['manga', 'morango', 'uva', 'maçã'];
+fruit.unshift('goiaba');
+console.log(fruit);
+
